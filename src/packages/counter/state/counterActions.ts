8582d3b3ac44dedfd9e4d86f3f2e72b8ebeb876e
ShirShintel <shirsh@wix.com>
2021-06-30 12:26:49 +0300

    counter actions

---
+++ src/packages/counter/state/counterActions.ts
@@ -0,0 +1,18 @@
+import { AnyAction } from 'redux';
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export interface CounterActions {
+    incrementCounter: () => AnyAction;
+    decrementCounter: () => AnyAction;
+}
+
+export const CounterActions:CounterActions = {
+    incrementCounter: (): AnyAction => ({
+        type: INCREMENT
+    }),
+    decrementCounter: (): AnyAction => ({
+        type: DECREMENT
+    })
+};
