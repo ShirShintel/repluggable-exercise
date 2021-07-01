9150ede4d750773dddf8e3c0cb6492e3d8aa87b6
ShirShintel <shirsh@wix.com>
2021-07-01 11:42:37 +0300

    finished with counter entry points

--- src/packages/counter/entryPoints/counterUIEntryPoint.tsx
+++ src/packages/counter/entryPoints/counterUIEntryPoint.tsx
@@ -1,28 +1,25 @@
-import { EntryPoint } from 'repluggable';
+import { EntryPoint } from "repluggable";
 import { MainViewAPI } from "../../mainView";
-import { TopBarAPI } from '../../topBar/topBarAPI';
-import { CounterAPI } from '../counterAPI';
-import { createConnectedDisplayer } from '../components/connected/displayer';
-import { createConnectedControls } from '../components/connected/controls';
+import { TopBarAPI } from "../../topBar/topBarAPI";
+import { CounterAPI } from "../counterAPI";
+import { createConnectedDisplayer } from "../components/connected/displayer";
+import { createConnectedControls } from "../components/connected/controls";
 
 export const CounterUIEntryPoint: EntryPoint = {
-    name: 'Counter UI Entry Point',
+  name: "Counter UI Entry Point",
 
-    getDependencyAPIs() {
-        return [MainViewAPI,TopBarAPI, CounterAPI];
-    },
+  getDependencyAPIs() {
+    return [MainViewAPI, TopBarAPI, CounterAPI];
+  },
 
-    extend(shell) {
-        const mainViewAPI = shell.getAPI(MainViewAPI);
-        const topBarAPI = shell.getAPI(TopBarAPI);
-        
-        const Displayer = createConnectedDisplayer(shell);
-        const Controls = createConnectedControls(shell);
+  extend(shell) {
+    const mainViewAPI = shell.getAPI(MainViewAPI);
+    const topBarAPI = shell.getAPI(TopBarAPI);
 
-        mainViewAPI.contributeComponent(shell, {component: () => <Displayer/>});
-        topBarAPI.contributeComponent(shell, {component: () => <Controls/>});
-    }
+    const Displayer = createConnectedDisplayer(shell);
+    const Controls = createConnectedControls(shell);
+
+    mainViewAPI.contributeComponent(shell, { component: () => <Displayer /> });
+    topBarAPI.contributeComponent(shell, { component: () => <Controls /> });
+  },
 };
-
-
-
